83b841759d99183f580381c1d5b0c5925872dcda
@samsesz <@samsesz>
2026-08-17 19:47:20 -0300

    fix: package

--- api/controllers/rankingController.js
+++ api/controllers/rankingController.js
@@ -91,4 +91,5 @@ export default {
   deleteRanking,
   updateRanking,
   getOneRanking,
+  getRankingOrdenado,
 };
--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -10,48 +9,27 @@ const nextConfig = {
   turbopack: {
     root: __dirname,
   },
+  allowedDevOrigins: ["192.168.1.66"],
 
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*",
       },
       {
-        source: '/uploads/:path*',
-        destination: 'http://localhost:4000/uploads/:path*',
+        source: "/uploads/:path*",
+        destination: "http://localhost:4000/uploads/:path*",
+      },
+      {
+        source: "/uploads/modelagens/:path*",
+        destination: "http://localhost:3002/uploads/modelagens/:path*",
+      },
+      {
+        source: "/uploads/midias/:path*",
+        destination: "http://localhost:3002/uploads/midias/:path*",
       },
     ];
-=======
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactCompiler: true,
-  turbopack: {
-    root: __dirname,
-  },
-  allowedDevOrigins: ['192.168.1.66'],
-
-  // Redirecionar requests para arquivos estáticos do backend
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: '/uploads/modelagens/:path*',
-          destination: 'http://localhost:3002/uploads/modelagens/:path*',
-        },
-        {
-          source: '/uploads/midias/:path*',
-          destination: 'http://localhost:3002/uploads/midias/:path*',
-        },
-      ],
-    };
->>>>>>> 8f09dddf1cd08ecfd8d7c1cc5ee3c7b1a3921dce
   },
 };
 
